fa906b3ae6ca2d56e74fd9db49e96ee8c96cb86a
zhouyi <zhouyi@ymatou.com>
2018-05-23 16:32:54 +0800

    add domain

--- pcount.sh
+++ pcount.sh
@@ -4,5 +4,6 @@ ps -ef|grep java |awk '{print $2}'|xargs ps hH p >temp2
 for pid in `cat temp1`
 do
     pline=`cat temp2|grep $pid|wc -l`
-    echo $pid $pline
+    domain=`jps -lvm|grep $pid|grep -P '(?<=log/).*(?=/gc.log)' -o`
+    echo $pid $pline $domain
 done
